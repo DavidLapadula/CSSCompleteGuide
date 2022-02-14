8b63e3783542b1483f10c4e44c4d7274ff0e3975
DavidLapadula <lapadula.david@gmail.com>
2022-02-14 11:57:55 -0500

    [responsive] added media queries for responsive design

---
+++ JsCss/js-css/shared.js
@@ -0,0 +1,25 @@
+// DOM parsed to create viruutal representation so they can be operated on
+// query selector argument can be targetted in the same way as css, including stacking and combinators
+// it will always select the first one, if need array of all of class use querySelectorAll
+// style property on selector will work with inline styles to ensure specificity is high enough
+
+var backdrop = document.querySelector(".backdrop");
+var model = document.querySelector(".modal");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+// anonymous function to attach event listener to each button
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    // model.style.display = "block";
+    // backdrop.style.display = "block";
+
+    // classname just a string that represents all classes. need to ADD style to prevent overwriting
+    modal.className = "block";
+    modal.classList.add("block"); // this only adds the class while leaving the rest intact. remove() also available
+  });
+}
+
+// notation differs in DOM object because object name cannot have a dash like CSS classes
+// if you want to use the normal CSS name then access with bracket notation like object['property-name']
